135c8c73df3071d9ad6ab98d01b88ae36bb6901f
Puran Singh <85459980+purandixit07@users.noreply.github.com>
2021-12-15 15:59:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+import pickle
+from wsgiref import simple_server
+from flask import Flask, request, app
+from flask import Response
+from flask_cors import CORS
+import pandas as pd
+
+app = Flask(__name__)
+CORS(app)
+app.config['DEBUG'] = True
+
+
+@app.route("/predict", methods=['POST'])
+def predictRoute():
+    try:
+        if request.json['data'] is not None:
+            data = request.json['data']
+            print('data is:     ', data)
+            res = predict_log(data)
+            print('result is        ', res)
+            return Response(res)
+    except ValueError:
+        return Response("Value not found")
+    except Exception as e:
+        print('exception is   ', e)
+        return Response(e)
+
+
+def predict_log(dict_pred):
+    with open("modelForPredRandForest.sav", 'rb') as f:
+        model = pickle.load(f)
+
+    data_df = pd.DataFrame(dict_pred, index=[1, ])
+    predict = model.predict(data_df)
+    return predict
+
+
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    port = 5000
+    app.run(debug=True)
+    # httpd = simple_server.make_server(host, port, app)
+    # print("Serving on %s %d" % (host, port))
+    # httpd.serve_forever()
